1fb1bbaae103df824121a2d65e20723eeae62ad6
Henri De Bel <henri.dbel@gmail.com>
2019-07-09 22:58:00 +0200

    adding initial files

---
+++ javascripts/app.js
@@ -0,0 +1,15 @@
+// Rover Object Goes Here
+// ======================
+
+// ======================
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
